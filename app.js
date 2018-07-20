8c5b91f559f9857ea52a2d16f220f3d9a3816727
Matthew Feng <mattfeng@mit.edu>
2018-07-19 22:13:37 -0400

    Add error

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500)
     res.render('error')
   } else {
+    res.status(err.status || 500)
     res.render('error-public')
   }
 })
